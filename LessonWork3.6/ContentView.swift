c366058ad52cef11d1580203eb8084719d39c22c
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2022-03-14 17:25:27 +0300

    1.0

--- LessonWork3.6/ContentView.swift
+++ LessonWork3.6/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            AwardsView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Awards")
+                }
+            MainView()
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Main")
+                }
+        }
     }
 }
 
